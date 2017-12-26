e7cbebb6f33a49f3cab56eed0fc0325eb4e0cbf0
Michael Hemingway <mikemingway@gmail.com>
2017-12-26 12:35:46 -0500

    misc

--- server/index.php
+++ server/index.php
@@ -45,11 +45,10 @@ if (isset($_SERVER['HTTP_ORIGIN'])) {
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
 
 	if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-		header("Access-Control-Allow-Methods: GET, POST, OPTIONS");         
+		header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 
 	if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
 		header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-
 	//exit(0);
 }
 

7c9dfe49d375ff2df9b6fa8d8dd22ac1ea5382b2
Michael Hemingway <mikemingway@gmail.com>
2017-12-30 12:47:52 -0500

    Better template page

--- desktop/src/renderer/components/_TemplatePage.vue
+++ desktop/src/renderer/components/_TemplatePage.vue
@@ -1,10 +1,21 @@
 <template>
+	<div>
+	</div>
 </template>
 
 <script>
-
 export default {
-	name: 'today-page'
+	name: 'template',
+	computed: {},
+	created () {
+		// axios.get('users/0').then(response => {
+		// 	this.account = response.data
+		// }).catch(e => { this.account = { user_nicename: ' ' } })
+
+		this.$on('tick', () => {})
+	},
+	data: () => ({}),
+	methods: {}
 }
 </script>
 

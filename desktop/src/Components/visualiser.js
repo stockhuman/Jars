39a61994a433bdb3c0db5bbc8248e0090d46e71d
Michael Hemingway <mpghemingway@gmail.com>
2019-07-10 11:27:38 -0400

    First fully functional release

--- desktop/src/Components/visualiser.js
+++ desktop/src/Components/visualiser.js
@@ -1,8 +1,17 @@
+// TODO: hardcoded strings
+const _scales = [
+	'a year',
+	'half a year',
+	'four months',
+	'three months',
+	'this month'
+]
+
 class Visualiser {
 	constructor({ root = null }) {
 		this.state = {
 			year: new Date().getFullYear(),
-			scale: 4, // 12 months / [scale] => 3 months
+			scale: 4,
 			root,
 			strings: locales('logform'),
 			svg: null,
@@ -25,8 +34,7 @@ class Visualiser {
 		this.state.container.appendChild(this.state.meta)
 		this.state.root.appendChild(this.state.container)
 
-		this.state.meta.addEventListener('click', () => this.setScale( ((this.state.scale + 1) % 13) || 1))
-
+		this.state.meta.addEventListener('click', () => this.setScale( this.state.scale + 1))
 
 		this.render()
 	}
@@ -39,7 +47,7 @@ class Visualiser {
 
 	setScale (scale) {
 		this.state.container.className = ''
-		this.state.scale = scale
+		this.state.scale = scale % _scales.length
 		this.render()
 	}
 
@@ -48,7 +56,7 @@ class Visualiser {
 		if (!window.api) return
 
 		// defines the query to be sent according to scaled view over time
-		const scale = () => {
+		const makeQuery = () => {
 			const monthsBack = months => {
 				let str = ''
 				for (let i = 0; i < months; i++) {
@@ -60,86 +68,65 @@ class Visualiser {
 			}
 
 			switch (this.state.scale) {
-				case 1: return `filter=date,sw,${this.state.year}` // whole year
-				case 2: return monthsBack(6)
-				case 3: return monthsBack(4)
-				case 4: return monthsBack(3)
-				case 5:
-				case 6: this.state.scale = 6; return monthsBack(2)
-				case 7:
-				case 8:
-				case 9:
-				case 10:
-				case 11:
-				case 12:
-					this.state.scale = 12
-					return `filter=date,sw,${SQLDate(new Date(this.state.year, new Date().getMonth())).slice(0, 7)}`
+				case 0: return `filter=date,sw,${this.state.year}` // whole year
+				case 1: return monthsBack(6)
+				case 2: return monthsBack(4)
+				case 3: return monthsBack(3)
+				case 4:
+					return `filter=date,sw,${SQLDate(
+						new Date(
+							this.state.year,
+							new Date().getMonth()
+						)).slice(0, 7)
+						}`
 			}
 		}
+
+		// returns distance in days
+		const dist = (a, b) => Math.abs(Math.round(((a - b) / (1000 * 60 * 60 * 24))))
 
 		// construct a single log <rect />
 		const day = day => {
 			let y = 0
-
-			const s1s = this.state.strings.s1.values
+			let x = ratio * dist(furthest, new Date(day.date))
+			let w = ratio * day.hours
 
 			// determine y given svg viewBox height of 105
 			switch (day.tod) {
-				case s1s[0].abbr: y = 10; break // in the early morning
-				case s1s[1].abbr: y = 20; break // in the morning
-				case s1s[2].abbr: y = 30; break // around midday
-				case s1s[3].abbr: y = 40; break // in the afternoon
-				case s1s[4].abbr: y = 50; break // in the evening
-				case s1s[5].abbr: y = 60; break // around nighttime
-				case s1s[6].abbr: y = 70; break // well past sundown
+				case todstrs[0].abbr: y = 10; break // in the early morning
+				case todstrs[1].abbr: y = 20; break // in the morning
+				case todstrs[2].abbr: y = 30; break // around midday
+				case todstrs[3].abbr: y = 40; break // in the afternoon
+				case todstrs[4].abbr: y = 50; break // in the evening
+				case todstrs[5].abbr: y = 60; break // around nighttime
+				case todstrs[6].abbr: y = 70; break // well past sundown
 			}
 
-			// TODO: fix the X positioning, reply to FB folk
-			// x = date relative scale
-			// width = day.hours relative scale => what about nights?
-
-			function diff_hours(dt2, dt1) {
-				let diff = (dt2.getTime() - dt1.getTime()) / 1000
-				diff /= (60 * 60)
-				return Math.abs(Math.round(diff))
-			}
-
-			/**
-			 * 1. find furthest date
-			 * 2. convert that date into hours from present (diff_hours())
-			 * 3. day.hours / 2392 = width / 600 => width
-			 */
-
-
-			const totalHours = diff_hours(new Date(), furthest)
-			const width = (day.hours / totalHours) * 600 * 20
-			const x = 600 - diff_hours(new Date(day.date), furthest) / 600
-
-			return `<rect x="${x}" y="${y}" width="${width}" height="3" rx="1.5" fill="#eee" class="${day.category}"/>`
+			return `<rect x="${x}" y="${y}" width="${w}" height="3" rx="1.5" class="${day.category}"/>`
 		}
 
 		// Query database for logs in date range
-		const query = `?${scale()}&exclude=id,comment,task`
+		const query = `?${makeQuery()}&exclude=id,comment,task`
 		const data = await fetch(window.api + query).then(r => r.json())
-
-		scale()
-
-		// determine positioning variables
-		const furthest = new Date(
-			this.state.year,
-			new Date().getMonth() - (12 / this.state.scale) || 11
-		)
-
-		console.log(furthest,
-			new Date().getMonth() + ' - ' +  ((12 / this.state.scale) )
-		)
+		const todstrs = this.state.strings.s1.values
 
 		let svg = ''
+		let fmonth = new Date().getMonth()
+		let furthest
 
-		// create demarcation lines
-		for (let i = 0; i < 12 / this.state.scale; i++) {
-			const x = i * (600 / this.state.scale) -1
-			svg += `<line x1="${x}" x2="${x}" y1="0" y2="100" stroke="var(--b-high)" style="opacity: 0.2"/>`
+		switch (this.state.scale) {
+			case 0: furthest = new Date(this.state.year, fmonth - 12); break
+			case 1: furthest = new Date(this.state.year, fmonth - 6); break
+			case 2: furthest = new Date(this.state.year, fmonth - 4); break
+			case 3: furthest = new Date(this.state.year, fmonth - 3); break
+			case 4: furthest = new Date(this.state.year, fmonth); break
+		}
+
+		let ratio = 600 / dist(furthest, new Date())
+
+		// create 'time of day' labels
+		for (let i = 0; i < 7; i++) {
+			svg += `<text x="0" y="${i * 10 + 15}">${todstrs[i].abbr}</text>`
 		}
 
 		// append logs to svg
@@ -158,8 +145,8 @@ class Visualiser {
 		}
 
 		// most commonly entered time of day for work
-		const modalTod = `Scale: 12/${this.state.scale}. Mode: ${mode(data.records.map(item => item.tod))}.<br>
-		From ${SQLDate(furthest).slice(0, 10)} to ${SQLDate().slice(0, 10)}`
+		const modalTod =
+			`Scale: ${_scales[this.state.scale]}. Mode: ${mode(data.records.map(item => item.tod))}.`
 
 		this.state.container.className = 'ready'
 		this.state.svg.innerHTML = svg

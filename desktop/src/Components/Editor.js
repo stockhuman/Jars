f12a27b4244d1c555131928d7478b470cbe6108e
Michael Hemingway <mpghemingway@gmail.com>
2019-08-05 16:28:10 -0400

    Editor edits using PUT

--- desktop/src/Components/Editor.js
+++ desktop/src/Components/Editor.js
@@ -2,7 +2,8 @@ class Editor extends Module {
 	constructor (props) {
 		super(props)
 		this.state = {
-			year: new Date().getFullYear()
+			year: new Date().getFullYear(),
+			modified: []
 		}
 		this.pill(props.click.x, props.click.y)
 		this.render()
@@ -21,8 +22,14 @@ class Editor extends Module {
 	}
 
 	hide () {
+		if (this.state.modified.length > 0) {
+			this.state.modified.forEach((log, id) => this.confirm(id, log))
+			this.state.closebtn.innerHTML = 'close'
+			this.state.modified = []
+		}
 		this.state.pill.classList = 'closed'
 		this.root.style.display = 'none'
+		this.root.classList = ''
 		this.state.closebtn.style.display = 'none'
 	}
 
@@ -30,13 +37,36 @@ class Editor extends Module {
 	show () {
 		this.state.pill.classList = ''
 		this.root.style.display = 'block'
+		this.root.classList = 'loaded'
 		this.state.closebtn.style.display = 'block'
 	}
 
-	events () {}
-
 	alter (data) {
-		console.log(data)
+		data.id.style.color = '#F29E74'
+		this.state.closebtn.innerHTML = 'update'
+		this.state.modified[Number(data.id.innerHTML)] = {
+			category: data.cat.innerHTML,
+			comment: data.comment.innerHTML,
+			date: data.date.innerHTML,
+			hours: data.hours.innerHTML,
+			project: data.proj.innerHTML,
+			task: data.task.innerHTML,
+			tod: data.tod.innerHTML
+		}
+	}
+
+	confirm (id, log) {
+		console.log(`Updated log #${id} with fields:`, log)
+
+		fetch(`${window.api}/${id}` , {
+			method: 'PUT',
+			// 	mode: 'cors', // no-cors, cors, *same-origin
+			// 	credentials: 'same-origin', // include, *same-origin, omit
+			headers: { 'Content-Type': 'application/json' },
+			referrer: 'no-referrer',
+			body: JSON.stringify(log),
+		})
+		.then(response => console.log(response.json()))
 	}
 
 	// This method adapts a similar one seen in Visualiser.js
@@ -58,36 +88,35 @@ class Editor extends Module {
 		const query = `?$&filter=date,bt,${start},${end}`
 		const data = await fetch(window.api + query).then(r => r.json())
 
-		const container = elem('section')
+		data.records.reverse() // display nearest logs first
+
+		const container = elem('section', {className: 'editor-log-list'})
 
 		data.records.forEach(l => {
 			// create individual, editable portions of each log
 			const li = elem('div', { className: 'editor-log' })
-			const config = { attributes: true, childList: true, subtree: true }
 
-			const parts = [
-				{ id: elem('span', { innerHTML: ("0000" + l.ID).substr(-4, 4), className: 'id' }) },
-				{ date: elem('span', { contentEditable: true, innerHTML: l.date, className: 'date' }) },
-				{	hours : elem('span', { contentEditable: true, innerHTML: l.hours, className: 'hours' }) },
-				{	proj: elem('span', { contentEditable: true, innerHTML: l.project, className: 'project' }) },
-				{ tod: elem('span', { contentEditable: true, innerHTML: l.tod, className: 'tod' }) },
-				{ task: elem('span', { contentEditable: true, innerHTML: l.task, className: 'task' }) },
-				{ cat: elem('span', { contentEditable: true, innerHTML: l.category, className: 'category' }) },
-				{ comment: elem('span', { contentEditable: true, innerHTML: l.comment, className: 'comment' }) }
-			]
+			const parts = {
+				id: elem('span', { innerHTML: ("0000" + l.ID).substr(-4, 4), className: 'id' }),
+				date: elem('span', { contentEditable: true, innerHTML: l.date, className: 'date' }),
+				hours: elem('span', { contentEditable: true, innerHTML: l.hours, className: 'hours' }),
+				proj: elem('span', { contentEditable: true, innerHTML: l.project, className: 'project' }),
+				tod: elem('span', { contentEditable: true, innerHTML: l.tod, className: 'tod' }),
+				task: elem('span', { contentEditable: true, innerHTML: l.task, className: 'task' }),
+				cat: elem('span', { contentEditable: true, innerHTML: l.category, className: 'category' }),
+				comment: elem('span', { contentEditable: true, innerHTML: l.comment, className: 'comment' })
+			}
 
-			parts.forEach(el => {
-				const e = Object.values(el)[0]
-				li.appendChild(e)
-				e.addEventListener('input', () => {this.alter(parts)})
+			Object.values(parts).forEach(el => {
+				li.appendChild(el)
+				el.addEventListener('input', () => this.alter(parts))
 			})
 
-			const observer = new MutationObserver(() => { this.alter(parts) })
-			observer.observe(li, config)
 			container.appendChild(li)
 		})
 
-		this.root.innerHTML = ''
+		this.root.innerHTML = `<h3>${data.records.length} logs from ${start} - ${end}</h3>`
 		this.root.appendChild(container)
+		this.root.classList = 'loaded'
 	}
 }

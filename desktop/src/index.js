77b49c5161f5572cf00b99faa51662dc2f0e8e58
Michael Hemingway <mpghemingway@gmail.com>
2020-05-05 23:55:54 -0400

    Fix deprecation notice

--- desktop/src/index.js
+++ desktop/src/index.js
@@ -38,8 +38,10 @@ const createWindow = () => {
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
     mainWindow = null;
-  });
-};
+  })
+}
+
+app.allowRendererProcessReuse = true
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
